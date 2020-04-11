28ce589105a9e2b84ace53ab3ee49ba65f958eb3
Lucas Lima <lucas.eng.lds@gmail.com>
2020-04-11 16:16:54 -0300

    {Estrutura} Adicionar page dashboardLider e rota

---
+++ web/src/pages/DashboardLider/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function DashboardLider() {
+  return (
+    <div />
+  );
+}
--- web/src/routes.js
+++ web/src/routes.js
@@ -3,11 +3,14 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 
+import DashboardLider from './pages/DashboardLider';
+
 export default function Routes(){
   return(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={LandingPage} />
+        <Route path="/lider/dashboard" component={DashboardLider} />
       </Switch>
     </BrowserRouter>
   )
