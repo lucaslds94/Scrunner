5a9f52c285b66377ce762f7e8b91fc3305b65d26
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2020-04-13 18:27:27 -0300

    {Dashboard Lider} Adicionar componente de gráfico

---
+++ web/src/components/RoundGraph/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Chart from "react-apexcharts";
+
+import "./styles.css";
+
+export default function RoundGraph({
+  isPercent = true,
+  complete = 0,
+  total,
+  title,
+  description,
+}) {
+  
+  const caculatePercent = (total, complete) => {
+    let result = (complete * 100) / total;
+
+    return result;
+  };
+
+  const OPTIONS_GRAPH = {
+    series: [isPercent ? complete : caculatePercent(total, complete)],
+    options: {
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            size: "50%",
+          },
+          track: {
+            background: "#B2B2B2",
+          },
+          dataLabels: {
+            name: {
+              show: false,
+            },
+            value: {
+              show: true,
+              fontSize: "35px",
+              fontWeight: "bold",
+              color: "#737FF3",
+              formatter: (val) => (isPercent ? `${val}%` : complete),
+            },
+          },
+        },
+      },
+      colors: ["#737FF3"],
+      stroke: {
+        lineCap: "round",
+      },
+    },
+  };
+
+  return (
+    <div className="graph-container">
+      <h3 className="graph-title">{title}</h3>
+      <div className="graph">
+        <Chart
+          options={OPTIONS_GRAPH.options}
+          series={OPTIONS_GRAPH.series}
+          type="radialBar"
+          width={325}
+        />
+      </div>
+      <p className="graph-description">{description}</p>
+    </div>
+  );
+}
