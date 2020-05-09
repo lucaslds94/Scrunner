d66051a8fe1343c05e0f2b7d0fe8589d908d914c
Julianne <julivolotao@gmail.com>
2020-05-08 22:10:35 -0300

    {Backend} Adicionar model DailyContent

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const Team = require('../models/Team');
 const UserTeam = require('../models/UserTeam');
 const DailyBoard = require('../models/DailyBoard');
+const DailyContent = require('../models/DailyContent');
 
 const connection = new Sequelize(dbConfig);
 
@@ -12,9 +13,11 @@ User.init(connection);
 Team.init(connection);
 UserTeam.init(connection);
 DailyBoard.init(connection);
+DailyContent.init(connection);
 
 UserTeam.associate(connection.models);
 DailyBoard.associate(connection.models);
+DailyContent.associate(connection.models);
 
 
 module.exports = connection;
---
+++ backend/src/models/DailyContent.js
@@ -0,0 +1,58 @@
+const { Model, DataTypes } = require('sequelize');
+
+class DailyContent extends Model {
+    
+    static init(sequelize) {
+        super.init({
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            did_yesterday: {
+                type: DataTypes.STRING(255)
+            },
+            do_today: {
+                type: DataTypes.STRING(255)
+            },
+            problems: {
+                type: DataTypes.STRING(255)
+            },
+            users_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "users",
+                    key: "id"
+                }
+            },
+            daily_boards_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "daily_boards",
+                    key: "id"
+                }
+            }
+
+        }, {
+            sequelize
+        })
+    }
+
+
+    static associate(models){
+        this.belongsTo(models.User, {
+            foreignKey: 'users_id',
+            as: 'fk_daily_content_users'
+        });
+
+        this.belongsTo(models.DailyBoard, {
+            foreignKey: 'daily_boards_id',
+            as: 'fk_daily_content_daily_board'
+        });
+    }
+}
+
+module.exports = DailyContent;
