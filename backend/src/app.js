841659a22f955dfb2de4c16fb6fbfdfdc8605aa6
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2020-05-09 10:16:50 -0300

    {Backend} Configurar testes
    Co-authored-by: lucaslds94 <lucas.eng.lds@gmail.com>

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
-require('dotenv').config();
-require('./database/');
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
+
+require("./database/");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const userRouter = require('./routes/user.router');
+const userRouter = require("./routes/user.router");
 
 app.use(cors());
 app.use(express.json());
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,11 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+});
 
 const config = {
-  dialect: "postgres",
-  host: process.env.DB_HOST ||"localhost",
+  dialect: process.env.DB_DIALECT || "postgres",
+  storage: './__tests__/database.sqlite',
+  host: process.env.DB_HOST || "localhost",
   database: "scrunner",
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PASS || "",
